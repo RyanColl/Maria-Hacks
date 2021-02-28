0dcdb26ad6cc518c291955ce07f9c92e99461741
Ryan <ryancollicutt22@gmail.com>
2021-02-28 01:57:39 -0800

    next push - getting tired

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 
+import './main.css';
 
-class GridLayout extends React.Component {
+export class GridLayout extends React.Component {
     constructor(props:any) {
         super(props);
     }
@@ -10,6 +11,7 @@ class GridLayout extends React.Component {
             <div id="wrapper">
 
                 <NavBar />
+                <Main date={date()}/>
 
 
             </div>
@@ -21,6 +23,9 @@ class GridLayout extends React.Component {
     }
 }
 
+let date = () => {
+    return Date();
+}
 
 
 class NavBar extends React.Component {
@@ -29,15 +34,45 @@ class NavBar extends React.Component {
     }
     render() {
         return(   
-                  <ul> 
-                    <li><a><img src={"./img/LogoMakr-4Ocmnm.png"} ></img></a></li>
-                    <li><button type="submit" value="hostroom"></button></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                  </ul>
+                  <nav>
+                    <div className="logo"><a href="#"><img id="logo" src={"./img/LogoMakr-4Ocmnm.png"} ></img></a></div>
+                    <div className="div2"><button className="hostroom" type="submit" value="hostroom">Host Room</button></div>
+                    <div className="div3"><button className="joinroom" type="submit" value="joinroom">Join Room</button></div>
+                    <div className="div4"><button className="newnote" type="submit" value="newnote">New Note</button></div>
+                  </nav>
                   
         );
       }
   }
 
+
+  const Main = (props: any) => {
+    
+    
+        return(
+            <main>
+                <div id="colours">
+                    <a href="1"><div id="circle1"></div></a>
+                    <a href="2"><div id="circle2"></div></a>
+                    <a href="3"><div id="circle3"></div></a>
+                    <a href="4"><div id="circle4"></div></a>
+                    <a href="5"><div id="circle5"></div></a>
+                    <a href="6"><div id="circle6"></div></a>
+                    <a href="7"><div id="circle7"></div></a>
+                </div>
+                <div id="notes">
+                    <div id="note1">
+                        <h6>{props.date}</h6>
+                        <p id="content">the start of your note would go here to remind you what you wrote...</p>
+                        <div id="butt">
+                            <a href="#"><button type="submit" value="edit">edit</button></a>
+                            <a href="#"><button type="submit" value="delete">delete</button></a>
+                        </div>
+                    </div>
+                    <div id="note2"></div>
+                    <div id="note3"></div>
+                </div>
+            </main>
+            );
+    
+  }
