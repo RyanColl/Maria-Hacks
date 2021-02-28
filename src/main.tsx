cadba3a87e420a1aaa67957b37867240aafe32da
Ryan <ryancollicutt22@gmail.com>
2021-02-27 19:26:39 -0800

    push for artin

---
+++ src/main.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+
+
+class GridLayout extends React.Component {
+    constructor(props:any) {
+        super(props);
+    }
+    render() {
+        return(
+            <div id="wrapper">
+
+                <NavBar />
+
+
+            </div>
+
+            
+
+            
+        )
+    }
+}
+
+
+
+class NavBar extends React.Component {
+    constructor(props:any) {
+        super(props);
+    }
+    render() {
+        return(   
+                  <ul> 
+                    <li><a><img src={"./img/LogoMakr-4Ocmnm.png"} ></img></a></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                  </ul>
+                  
+        );
+      }
+  }
+
+class 
