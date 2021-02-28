381924ab569bde661de31241e0df96b8eea82962
Ryan <ryancollicutt22@gmail.com>
2021-02-28 09:45:39 -0800

    deoploy push

--- src/main.tsx
+++ src/main.tsx
@@ -35,10 +35,10 @@ class NavBar extends React.Component {
     render() {
         return(   
                   <nav>
-                    <div className="logo"><a href="#"><img id="logo" src={"./img/LogoMakr-4Ocmnm.png"} ></img></a></div>
+                    <div className="logo"><a href="#"><img id="logo" src={"./img/LogoMakr-4Ocmnm1.png"} ></img></a></div>
                     <div className="div2"><button className="hostroom" type="submit" value="hostroom">Host Room</button></div>
                     <div className="div3"><button className="joinroom" type="submit" value="joinroom">Join Room</button></div>
-                    <div className="div4"><button className="newnote" type="submit" value="newnote">New Note</button></div>
+                    <div className="div4"><button id="newnote" className="newnote" type="submit" value="newnote">New Note</button></div>
                   </nav>
                   
         );
@@ -46,9 +46,13 @@ class NavBar extends React.Component {
   }
 
 
+
+ 
   const Main = (props: any) => {
-    
-    
+
+        
+
+
         return(
             <main>
                 <div id="colours">
@@ -61,16 +65,14 @@ class NavBar extends React.Component {
                     <a href="7"><div id="circle7"></div></a>
                 </div>
                 <div id="notes">
-                    <div id="note1">
+                    <div id="note">
                         <h6>{props.date}</h6>
                         <p id="content">the start of your note would go here to remind you what you wrote...</p>
                         <div id="butt">
-                            <a href="#"><button type="submit" value="edit">edit</button></a>
-                            <a href="#"><button type="submit" value="delete">delete</button></a>
+                            <a href="#"><button type="button" value="edit">edit</button></a>
+                            <a href="/*"><button type="button" value="delete">delete</button></a>
                         </div>
                     </div>
-                    <div id="note2"></div>
-                    <div id="note3"></div>
                 </div>
             </main>
             );
